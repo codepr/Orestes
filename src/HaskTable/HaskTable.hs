48d27233d5c5a0622a5af617e36a3867ae6b5a87
Andrea Giacomo Baldan <a.g.baldan@gmail.com>
2017-01-10 21:16:52 +0100

    first commit

--- src/HaskTable/HaskTable.hs
+++ src/HaskTable/HaskTable.hs
@@ -1,3 +1,26 @@
+{-
+Copyright (c) 2017 Andrea Giacomo Baldan
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-}
+
 module HaskTable.HaskTable (serveHashMap) where
 
 
@@ -34,7 +57,7 @@ wait sock store = do
     (client, _, _) <- accept sock
     putStrLn "Connection received"
     forkIO $ commandProcessor client store
-    wait sock store
+    -- wait sock store
 
 
 commandProcessor :: Handle -> Store -> IO ()
@@ -42,7 +65,7 @@ commandProcessor handle store = do
     line <- hGetLine handle
     let cmd = words line in
         case parseRequest cmd of
-          Left err -> return ()
+          Left err  -> return ()
           Right req -> do
               case processWith req store of
                 Left reply -> do
--- src/HaskTable/Parser.hs
+++ src/HaskTable/Parser.hs
@@ -1,3 +1,26 @@
+{-
+Copyright (c) 2017 Andrea Giacomo Baldan
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-}
+
 module HaskTable.Parser( Request(Put, Get, Del, Empty, Echo)
                        , Reply (store, content)
                        , parseRequest
@@ -14,13 +37,14 @@ data Request = Put String [String]
 data Reply = Reply { store :: Map.Map String [String]
                    , content :: String } deriving (Show)
 
--- Parse a request formed by a list of String, returning the correct command
+-- | Parse a request formed by a list of String, returning the correct command
+-- or an error if the command is unknown
 parseRequest :: [String] -> Either String Request
 parseRequest request =
     case (head request) of
-      ("put") -> Right (Put (head (tail request)) (drop 2 request))
-      ("get") -> Right (Get $ head (tail request))
-      ("del") -> Right (Del $ head (tail request))
-      ("echo") -> Right (Echo $ tail request)
+      ("put")   -> Right (Put (head (tail request)) (drop 2 request))
+      ("get")   -> Right (Get $ head (tail request))
+      ("del")   -> Right (Del $ head (tail request))
+      ("echo")  -> Right (Echo $ tail request)
       ("empty") -> Right (Empty)
       _ -> Left ("Error")
